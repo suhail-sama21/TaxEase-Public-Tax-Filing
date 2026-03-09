8384d124b42cffc40a150757152ee8def0ca6f45
suhail-sama21 <suhail.sama@cognizant.com>
2026-03-09 18:06:37 +0530

    "Pushing Project Structure"

---
+++ src/main/java/com/cognizant/taxease/TaxEaseApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.taxease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cognizant/taxease/TaxEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.taxease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
