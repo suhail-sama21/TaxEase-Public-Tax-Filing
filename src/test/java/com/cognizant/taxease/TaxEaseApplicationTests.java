1e5940e83b9542088215c478d375e51e4d68e1cc
giridharang2-lang <Giridharan.G2@cognizant.com>
2026-03-17 14:19:30 +0530

    Step 4: Final Validation - Compile: SUCCESS, Tests: 1/1 passed

--- src/test/java/com/cognizant/taxease/TaxEaseApplicationTests.java
+++ src/test/java/com/cognizant/taxease/TaxEaseApplicationTests.java
@@ -15,8 +15,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.batch.jdbc.initialize-database=never",
+    "spring.batch.job.enabled=false"
+})
 class TaxEaseApplicationTests {
     @Autowired
     private UserRepository userRepository;
