741a617dfc0f2ef51185035b35682b0162e0ef49
demo <2478168@cognizant.com>
2026-03-18 16:51:58 +0530

    create, read all , read one , update , auto create audit when result = "Non-Compliant", Compliance TFRMS-51

--- src/main/java/com/cognizant/taxease/controller/ComplianceController.java
+++ src/main/java/com/cognizant/taxease/controller/ComplianceController.java
@@ -1,10 +1,12 @@
 package com.cognizant.taxease.controller;
 
+import com.cognizant.taxease.dto.ComplianceResponse;
 import com.cognizant.taxease.dto.CreateComplianceRequest;
 import com.cognizant.taxease.entity.ComplianceRecord;
 import com.cognizant.taxease.service.ComplianceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import com.cognizant.taxease.dto.UpdateComplianceRequest;
 
 import java.util.List;
 
@@ -16,12 +18,22 @@ public class ComplianceController {
     private final ComplianceService complianceService;
 
     @PostMapping
-    public ComplianceRecord createCompliance(@RequestBody CreateComplianceRequest request) {
+    public ComplianceResponse createCompliance(@RequestBody CreateComplianceRequest request) {
         return complianceService.createCompliance(request);
     }
 
     @GetMapping
-    public List<ComplianceRecord> getAllCompliance() {
+    public List<ComplianceResponse> getAllCompliance() {
         return complianceService.getAllCompliance();
+    }
+
+    @GetMapping("/{id}")
+    public ComplianceResponse getComplianceById(@PathVariable Long id){
+        return complianceService.getComplianceById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ComplianceResponse updateCompliance(@PathVariable Long id, @RequestBody UpdateComplianceRequest request) {
+        return complianceService.updateCompliance(id, request);
     }
 }
---
+++ src/main/java/com/cognizant/taxease/dto/UpdateComplianceRequest.java
@@ -0,0 +1,12 @@
+package com.cognizant.taxease.dto;
+import lombok.Data;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+
+public class UpdateComplianceRequest {
+    private String result;
+    private String notes;
+}
