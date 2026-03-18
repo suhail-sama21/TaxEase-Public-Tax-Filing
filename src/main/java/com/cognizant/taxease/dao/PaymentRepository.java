ae55f6cab86afea62a695fb68923c8dfe8364ea8
Suprajith <Suprajith.JE@cognizant.com>
2026-03-19 00:12:07 +0530

    demo

--- src/main/java/com/cognizant/taxease/dao/PaymentRepository.java
+++ src/main/java/com/cognizant/taxease/dao/PaymentRepository.java
@@ -2,6 +2,11 @@ package com.cognizant.taxease.dao;
 
 import com.cognizant.taxease.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    // Fulfills TAXFR-11: Get payments for a specific taxpayer
+    List<Payment> findByFiling_Taxpayer_Id(Long taxpayerId);
 }
--- src/main/java/com/cognizant/taxease/dao/RevenueRecordRepository.java
+++ src/main/java/com/cognizant/taxease/dao/RevenueRecordRepository.java
@@ -2,6 +2,8 @@ package com.cognizant.taxease.dao;
 
 import com.cognizant.taxease.entity.RevenueRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RevenueRecordRepository extends JpaRepository<RevenueRecord, Long> {
 }
