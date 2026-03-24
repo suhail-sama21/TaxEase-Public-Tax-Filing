64852c0c55e598766ca7766a6476bce1bffbb4bb
giridharang2-lang <Giridharan.G2@cognizant.com>
2026-03-24 09:00:53 +0530

    implemented test classes for module 2

--- src/main/java/com/cognizant/taxease/exception/DocumentVerificationException.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.taxease.exception;
-
-public class DocumentVerificationException extends RuntimeException {
-    public DocumentVerificationException(String message) {
-        super(message);
-    }
-}
